65c63c7e1b4f388c819aaf95bfb72562941d7899
Andrea AG <agandrea242@gmail.com>
2025-09-05 20:31:29 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -9,43 +9,42 @@ const regex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
 
 //Se crea una función que se encargue de añadir los amigos a la variable listaAmigos
 function agregarAmigo() {
+  //Inicializamos una variable para almacenar el valor del input
+  //Capturamos el valor del input por Id (amigo)
+  let amigo = document.getElementById("amigo").value;
 
-    //Inicializamos una variable para almacenar el valor del input
-    //Capturamos el valor del input por Id (amigo)
-    let amigo = document.getElementById('amigo').value;
-
-    //Validamos que el valor no sea nulo, ni vacío 
-    validarValorInput(amigo);
-
+  //Validamos que el valor no sea nulo, ni vacío
+  validarValorInput(amigo);
 }
 
 //Se crea una función para validar si el campo es nulo, vacío o no esta definido
 function validarValorInput(amigo) {
-    //Validamos que el campo no este vacio
-    if (!regex.test(amigo.trim())){
-        //Mostrar el alert
-        abrirAlerta('Por favor, inserte un nombre.');
-    }else{
-        //Insertar el nombre en la lista
-        listaAmigos.push(amigo.trim());
-        //Limpiar el input
-        limpiarInput();
-    }
+  //Validamos que el campo no este vacio
+  if (!regex.test(amigo.trim())) {
+    //Mostrar el alert
+    abrirAlerta("Por favor, inserte un nombre.");
+  } else {
+    //Insertar el nombre en la lista
+    listaAmigos.push(amigo.trim());
+    // Actualizar lista de amigos
+    actualizarListaAmigos();
+    //Limpiar el input
+    limpiarInput();
+  }
 }
 
 // Función encargada de crear y mostrar la alerta
 function abrirAlerta(mensaje) {
-
-    //Validar si el alert no existe
-    if (!miAlerta) {
-        // Crear div contenedor del alert
-        const alert = document.createElement("div");
-        // Asignar clases a la alerta
-        alert.className = "alert alert-info alert-dismissable clear in";
-        //Asignar Id a la alerta
-        alert.setAttribute('id', 'myAlert');
-        //Insertar estructura HTML de la alerta
-        alert.innerHTML = `
+  //Validar si el alert no existe
+  if (!miAlerta) {
+    // Crear div contenedor del alert
+    const alert = document.createElement("div");
+    // Asignar clases a la alerta
+    alert.className = "alert alert-info alert-dismissable clear in";
+    //Asignar Id a la alerta
+    alert.setAttribute("id", "myAlert");
+    //Insertar estructura HTML de la alerta
+    alert.innerHTML = `
             <div class="text">
                 <strong>Advertencia</strong>
                 <span>${mensaje}</span>
@@ -55,28 +54,44 @@ function abrirAlerta(mensaje) {
             </button>
         `;
 
-        // Insertar en el contenedor
-        document.getElementById("demo-preview").appendChild(alert);
-        // Eliminar la alerta después de 3 segundos
-        setTimeout(() => {
-            alert.remove(); // Elimina la alerta
-        }, 3000);
-    };
-    
+    // Insertar en el contenedor
+    document.getElementById("demo-preview").appendChild(alert);
+    // Eliminar la alerta después de 3 segundos
+    setTimeout(() => {
+      alert.remove(); // Elimina la alerta
+    }, 3000);
   }
+}
 
 //Función encargada de cerrar la alerta
 function cerrarAlerta() {
-    // Se elimina la clase y se transparenta la alerta
-    miAlerta.classList.remove("in");
-    // Elimina la alerta
-    setTimeout(() => {
-      miAlerta.remove(); 
-    }, 500);
+  // Se elimina la clase y se transparenta la alerta
+  miAlerta.classList.remove("in");
+  // Elimina la alerta
+  setTimeout(() => {
+    miAlerta.remove();
+  }, 500);
 }
 
 // Función encargada de limpiar el campo de texto
-function limpiarInput(){
-    //Restablecemos el valor del objeto a vacío
-    document.getElementById("amigo").value = "";
+function limpiarInput() {
+  //Restablecemos el valor del objeto a vacío
+  document.getElementById("amigo").value = "";
+}
+
+//Función para actualizar lista de amigos
+function actualizarListaAmigos() {
+  //Obtener el elemento lista
+  let lista = document.getElementById("listaAmigos");
+  // Limpiar la lista existente
+  lista.innerHTML = "";
+  //Iterar sobre el arreglo
+  for (let i = 0; i < listaAmigos.length; i++) {
+    //Se crea el objeto li
+    let li = document.createElement("li");
+    //Se asigna el contenido al li
+    li.textContent = listaAmigos[i];
+    //Agregar elementos a la lista
+    lista.appendChild(li);
+  }
 }
